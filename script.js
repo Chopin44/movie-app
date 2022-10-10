5dedd730e3c8617b3dc428380e204016d3f06440
Rodhi <Rodhi@gmail.com>
2022-10-10 18:57:36 +0700

    Search sudah bisa cuy

--- script.js
+++ script.js
@@ -2,24 +2,45 @@ let contain = document.getElementById("container")
 let inputSearch = document.querySelector("input")
 let search = document.querySelector("form")
 
-
-
-
-
-let getDataSearch = async () => {
-  search.addEventListener("submit", e => {
-    e.preventDefault()
+// Data pertama akan ditimpa data baru hasil search
+search.addEventListener("submit", e => {
+  e.preventDefault()
+  query = inputSearch.value
+  if (query == "") {
+    Swal.fire({
+      title: "Silahkan masukkan judul",
+      confirmButtonText: 'Oke'
+    })
+  } else {
+    let getDataSearch = async () => {
+      let html = ""
+      let getSearch = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=a648e8e7ea4db9c717c838225afd0372&query=${query}&page=`)
+      let dataSearch = await getSearch.json()
+      dataSearch.results.forEach(el => {
+        let htmlSegment = 
+          `<div class="col">
+          <div class="card h-100">
+          <img src="https://image.tmdb.org/t/p/original//${el.poster_path}" class="card-img-top" alt="unknown-image">
+            <div class="card-body">
+              <h5 class="card-title">${el.title}</h5>
+              <h6>Rate: ${el.vote_average}</h6>
+            </div>
+          </div>
+        </div>`
+        html += htmlSegment
+        contain.innerHTML = html   
+        
+      });
+      console.log(dataSearch);
+    }
+    getDataSearch()
+  }
   
-    a = inputSearch.value
-    return a
-  })
-  let getSearch = await fetch("https://api.themoviedb.org/3/search/movie?api_key=a648e8e7ea4db9c717c838225afd0372&query=lou&page=1")
-  let dataSearch = await getSearch.json()
-  console.log(dataSearch);
-}
+  
+  
+})
 
-getDataSearch()
-
+// Data pertama tampil
 fetch("https://api.themoviedb.org/3/discover/movie?api_key=a648e8e7ea4db9c717c838225afd0372&sort_by=popularity.desc&page=1")
 .then((Response) => {
     return Response.json()
@@ -32,10 +53,10 @@ fetch("https://api.themoviedb.org/3/discover/movie?api_key=a648e8e7ea4db9c717c83
             let htmlSegment = 
           `<div class="col">
           <div class="card h-100">
-          <img src="https://image.tmdb.org/t/p/original//${e.poster_path}" class="card-img-top" alt="...">
+          <img src="https://image.tmdb.org/t/p/original//${e.poster_path}" class="card-img-top" alt="unknown-image">
             <div class="card-body">
               <h5 class="card-title">${e.title}</h5>
-              <h5>Rate: ${e.vote_average}</h5>
+              <h6>Rate: ${e.vote_average}</h6>
             </div>
           </div>
         </div>`
@@ -46,6 +67,8 @@ fetch("https://api.themoviedb.org/3/discover/movie?api_key=a648e8e7ea4db9c717c83
         
     })
        
+}).catch(err =>{
+  alert("An error was occured", err)
 });
 
   
